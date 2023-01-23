976c0cda5c367c719fae13cc6bca7509b707987f
Mathews <mathewsrodrigues9@gmail.com>
2023-01-23 19:32:35 -0300

    corrigindo linto backend

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -6,9 +6,12 @@ const routers = require('../routes/index.router');
 const app = express();
 
 app.use((_req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
-  res.header("Access-Control-Allow-Headers", 'Origin, X-Requested-With, Content-Type, Authorization, application/json' );
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Authorization, application/json',
+  );
   next();
 });
 app.use(cors());
--- back-end/src/controllers/login.controller.js
+++ back-end/src/controllers/login.controller.js
@@ -1,11 +1,11 @@
-const loginService = require("../services/login.service");
+const loginService = require('../services/login.service');
 
 const login = async (req, res, next) => {
   const { email, password } = req.body;
 
   try {
     const { status, message } = await loginService.login(email, password);
-    return res.status(status).json({ message })
+    return res.status(status).json({ message });
   } catch (error) {
     next(error);
   }

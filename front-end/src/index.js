a0b9e27a7b9f48d245e3dc80239ec358f1b833e3
Bruna Eduarda Cruz Maciel <brunaduda37@gmail.com>
2023-01-24 18:10:05 -0300

    iniciado tela de produtos

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -4,12 +4,15 @@ import ReactDOM from 'react-dom';
 import reportWebVitals from './reportWebVitals';
 import './index.css';
 import App from './App';
+import Provider from './provider';
 
 ReactDOM.render(
   <React.StrictMode>
+    <Provider>
     <BrowserRouter>
       <App />
     </BrowserRouter>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root'),
 );

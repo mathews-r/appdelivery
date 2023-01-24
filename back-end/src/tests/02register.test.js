c3ade6b521108dd18058ea4480e21082867098dc
Mathews <mathewsrodrigues9@gmail.com>
2023-01-24 18:27:19 -0300

    corrigindo erros do teste

--- back-end/src/tests/02register.test.js
+++ back-end/src/tests/02register.test.js
@@ -6,22 +6,23 @@ const { User } = require('../database/models')
 chai.use(chaiHttp);
 
 const app = require("../api/app");
-const { mockInputCustomer, mockCustomer } = require("./mocks/mockRegister");
+const { mockCustomer } = require("./mocks/mockRegister");
 
 describe("Testes na rota /register", () => {
   describe("Testes com POST", () => {
     it("Cria um usuário com sucesso", async () => {
       sinon.stub(User, 'findOne').resolves(null);
-      sinon.stub(User, 'create').resolves(mockCustomer);
+      sinon.stub(User, 'create').resolves({ dataValues: mockCustomer[1] });
 
-      const response = await chai.request(app).post('/register').send(mockInputCustomer);
+      const response = await chai.request(app).post('/register').send(mockCustomer[0]);
+
       expect(response.status).to.be.eq(201);
-      expect(response.body).to.be.deep.eq(mockCustomer);
+      expect(response.body).to.be.deep.eq(mockCustomer[1]);
     });
     it("Falha ao tentar criar um usuário ja existente", async () => {
-      sinon.stub(User, 'findOne').resolves(mockCustomer);
+      sinon.stub(User, 'findOne').resolves(mockCustomer[1]);
 
-      const response = await chai.request(app).post('/register').send(mockInputCustomer);
+      const response = await chai.request(app).post('/register').send(mockCustomer[0]);
       expect(response.status).to.be.eq(409);
       expect(response.body.message).to.be.eq('User already registred');
     });

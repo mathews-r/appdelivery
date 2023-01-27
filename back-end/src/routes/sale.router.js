227723f9794f9a5531fe15f1e3b3d285ad1fd3f6
Mathews <mathewsrodrigues9@gmail.com>
2023-01-27 19:03:44 -0300

    criando rota para get na sales

--- back-end/src/routes/sale.router.js
+++ back-end/src/routes/sale.router.js
@@ -5,5 +5,6 @@ const { tokenValidation } = require('../middlewares/tokenValidation');
 const saleRouter = express.Router();
 
 saleRouter.post('/', tokenValidation, saleController.newSale);
+saleRouter.get('/', tokenValidation, saleController.getSaleByUser);
 
 module.exports = { saleRouter };

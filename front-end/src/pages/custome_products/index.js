0f2fd6cd43c03593a49e6663c885dba5b0baa407
Mathews <mathewsrodrigues9@gmail.com>
2023-01-27 14:47:16 -0300

    corigindo a rota do botao carrinho

--- front-end/src/pages/custome_products/index.js
+++ front-end/src/pages/custome_products/index.js
@@ -1,17 +1,19 @@
 import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import NavBar from '../../components/NavBar';
 import ProductCard from '../../components/ProductCard';
 import api from '../../service/request';
 
 function CustomerProducts() {
+  const navigate = useNavigate();
   const [products, setProducts] = useState([]);
   const [total, setTotal] = useState();
+  const [isActive, setIsActive] = useState(true);
 
   async function loadProducts() {
-    await api.get.getAllProducts()
-      .then(({ data }) => {
-        setProducts(data);
-      });
+    await api.get.getAllProducts().then(({ data }) => {
+      setProducts(data);
+    });
   }
 
   const getTotal = (storage) => {
@@ -38,7 +40,12 @@ function CustomerProducts() {
     <main>
       <NavBar />
 
-      <button type="button" data-testid="customer_products__button-cart">
+      <button
+        type="button"
+        data-testid="customer_products__button-cart"
+        disabled={ isActive }
+        onClick={ () => navigate('/customer/checkout') }
+      >
         <p>Ver carrinho: R$</p>
         <p data-testid="customer_products__checkout-bottom-value">
           {`${total ? total.toFixed(2).replace('.', ',') : 0}`}
@@ -46,19 +53,18 @@ function CustomerProducts() {
       </button>
       <section>
         <ul>
-          {
-            products.map((item, index) => (
-              <li key={ index }>
-                <ProductCard
-                  id={ item.id }
-                  image={ item.url_image }
-                  name={ item.name }
-                  price={ item.price.replace('.', ',') }
-                  handleCard={ (e) => handleCard(e) }
-                />
-              </li>
-            ))
-          }
+          {products.map((item, index) => (
+            <li key={ index }>
+              <ProductCard
+                id={ item.id }
+                image={ item.url_image }
+                name={ item.name }
+                price={ item.price.replace('.', ',') }
+                handleCard={ (e) => handleCard(e) }
+                setIsActive={ () => setIsActive() }
+              />
+            </li>
+          ))}
         </ul>
       </section>
     </main>

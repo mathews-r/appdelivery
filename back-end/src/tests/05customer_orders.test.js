529245a2b2dfe46f277a151bf580cd4cfab249e4
Mathews <mathewsrodrigues9@gmail.com>
2023-01-28 14:05:35 -0300

    ajustando test de sales

---
+++ back-end/src/tests/05customer_orders.test.js
@@ -0,0 +1,16 @@
+const sinon = require("sinon");
+const chai = require("chai");
+const { expect } = chai;
+const chaiHttp = require("chai-http");
+chai.use(chaiHttp);
+
+const app = require("../api/app");
+
+describe("", () => {
+  describe("", () => {
+    it("", async () => {});
+    it("", async () => {});
+    it("", async () => {});
+    afterEach(() => sinon.restore());
+  });
+});

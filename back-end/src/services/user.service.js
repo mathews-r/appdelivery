d501898c05cebb5b86e333044be89afb7b286472
Mateus Hoffman <mateushoffmandev@gmail.com>
2023-01-31 21:03:00 -0300

    lint

--- back-end/src/services/user.service.js
+++ back-end/src/services/user.service.js
@@ -73,7 +73,7 @@ const getUsers = async () => {
 };
 
 const deleteUser = async (id) => {
-  await User.destroy({where: {id}});
+  await User.destroy({ where: { id } });
 };
 
 module.exports = { validateLogin, login, newUser, getUsers, newAdminUser, deleteUser };
